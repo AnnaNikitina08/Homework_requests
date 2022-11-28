161281ebad6b742169d260882b3314660ccfe7be
Анна Л. Никитина <a.nikitina@lenoblpogspas.local>
2022-11-28 10:51:37 +0300

    Initial commit

---
+++ task1.py
@@ -0,0 +1,35 @@
+import requests
+
+base_url = "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api"
+
+hero_intelligence = {}
+
+def get_hero_list(hero_name):
+    uri = '/all.json'
+    request_url = base_url + uri
+    response = requests.get(request_url)
+    response.json()
+    for hero in response.json():
+        if hero ["name"] == hero_name:
+            intelligence = hero["powerstats"]["intelligence"]
+            hero_intelligence[hero_name] = int(intelligence)
+            print(hero_intelligence)
+
+get_hero_list("Hulk")
+get_hero_list("Captain America")
+get_hero_list("Thanos")
+
+int_max = max(list(hero_intelligence.values()))
+rev = dict(map(reversed, hero_intelligence.items()))
+pers_max = rev[int_max]
+print(f"Наибольший интеллект {int_max} у персонажа {pers_max}")
+
+
+
+
+
+
+
+
+
+
---
+++ task2.py
@@ -0,0 +1,44 @@
+import os
+import requests
+
+from settings import TOKEN
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    base_host = "https://cloud-api.yandex.net:443/"
+
+
+    def get_headers(self):
+        return {
+            'Content-type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
+    def _get_upload_link(self, file_path: str):
+         """Метод получает ссылку для загрузки файла на Яндекс-диск"""
+         uri = 'v1/disk/resources/upload/'
+         requests_url = self.base_host + uri
+         params = {'path': file_path, 'overwrite': True}
+         response = requests.get(requests_url, headers=self.get_headers(),
+                                params=params)
+         print(response.json())
+         return response.json()['href']
+
+    def upload_to_disk(self, path_to_file, file_path):
+         upload_url = self._get_upload_link(file_path)
+         response = requests.put(upload_url,
+                                 data=open(path_to_file, 'rb'),
+                                 headers=self.get_headers())
+         if response.status_code == 201:
+             print("Загрузка произошла успешно")
+
+
+
+if __name__ == '__main__':
+    """Получить путь к загружаемому файлу и токен от пользователя"""
+    path_to_file = "C:\\Документы для СЭД\Порт Батуми.jpg"
+    token = TOKEN
+    uploader = YaUploader(token)
+    uploader.upload_to_disk(path_to_file, "Порт Батуми")
